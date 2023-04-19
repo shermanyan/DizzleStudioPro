3aa008f4849b7239ab867f55d25128eda14855a0
Brandon Hargitay <93677930+BrandonHargitay@users.noreply.github.com>
2023-04-19 15:36:54 -0700

    Add StaticAudioVisualizer

--- cmake-build-debug/CMakeFiles/DizzleStudioPro.dir/App/UIComponents/SeekBar.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/DizzleStudioPro.dir/App/UIComponents/SeekBar.cpp.o: \
-  /Users/hargy/CLionProjects/CS008/DizzleStudioPro/App/UIComponents/SeekBar.cpp \
-  /Users/hargy/CLionProjects/CS008/DizzleStudioPro/App/UIComponents/SeekBar.h
