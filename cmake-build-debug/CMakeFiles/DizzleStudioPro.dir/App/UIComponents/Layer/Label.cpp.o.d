7c1ccaea5b8406294a0d06069b685dc498af760f
Brandon Hargitay <93677930+BrandonHargitay@users.noreply.github.com>
2023-04-19 21:54:28 -0700

    Fix Errors

--- cmake-build-debug/CMakeFiles/DizzleStudioPro.dir/App/UIComponents/Layer/Label.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/DizzleStudioPro.dir/App/UIComponents/Layer/Label.cpp.o: \
-  /Users/hargy/CLionProjects/CS008/DizzleStudioPro/App/UIComponents/Layer/Label.cpp \
-  /Users/hargy/CLionProjects/CS008/DizzleStudioPro/App/UIComponents/Layer/Label.h
--- cmake-build-debug/CMakeFiles/DizzleStudioPro.dir/App/UIComponents/Layer/Layer.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/DizzleStudioPro.dir/App/UIComponents/Layer/Layer.cpp.o: \
-  /Users/hargy/CLionProjects/CS008/DizzleStudioPro/App/UIComponents/Layer/Layer.cpp \
-  /Users/hargy/CLionProjects/CS008/DizzleStudioPro/App/UIComponents/Layer/Layer.h
