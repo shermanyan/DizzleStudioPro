dc07668f63eb53d7e0bbed5b93808eaf7477c9b1
Sherman Yan <syan1689@gmail.com>
2023-05-02 20:20:26 -0700

    zoom fixed

--- App/SplashScreen/SplashScreen.cpp
+++ App/SplashScreen/SplashScreen.cpp
@@ -23,7 +23,7 @@ void SplashScreen::setUpTextures() {
 
     musicMixer.setTexture(Textures::getTexture(MUSICMIXER));
     musicMixer.setSize({1000,600});
-    musicMixer.setPosition(15, 1490);
+    musicMixer.setPosition(25, 1490);
 
     musicPlayer.setTexture(Textures::getTexture(MUSICPLAYER));
     musicPlayer.setSize({1000,600});
@@ -37,39 +37,39 @@ void SplashScreen::setUpText() {
     startButton.setCharacterSize(55);
     startButton.setOrigin(startButton.getLocalBounds().width/2, startButton.getLocalBounds().height/2);
     startButton.setFillColor({151, 151,151});
-    startButton.setPosition(1000, 563);
+    startButton.setPosition(1000, 480);
 
     dizzleStudio.setFont(Fonts::getFont(NUNITO_BOLD));
-    dizzleStudio.setCharacterSize(100);
-    dizzleStudio.setPosition({450,390});
+    dizzleStudio.setCharacterSize(180);
     dizzleStudio.setString("Dizzle Studio");
     dizzleStudio.setFillColor({175, 143, 54});
     Position::center(dizzleStudio,startButton);
-    Position::top(dizzleStudio, startButton, 50);
+    Position::top(dizzleStudio, startButton, 100);
 
     credits.setFont(Fonts::getFont(NUNITO_BOLD));
-    credits.setFillColor({151, 151,151});
+    credits.setFillColor({94,94,94});
     credits.setString("Created By: Brandon Hargitay & Sherman Yan");
     credits.setPosition(2000 - credits.getLocalBounds().width -30, 1125 - 50);
 
     pro.setFont(Fonts::getFont(NUNITO_BOLD));
     pro.setString("PRO");
     pro.setFillColor({167, 42, 54});
-    pro.setCharacterSize(100);
-    pro.setPosition({dizzleStudio.getPosition().x + dizzleStudio.getGlobalBounds().width + 50, -1125});
+    pro.setCharacterSize(200);
+    pro.setPosition({0, -1125});
+    Position::right(pro,dizzleStudio,50);
     initialDizzleStudioPosition = dizzleStudio.getPosition();
 
     welcome.setFont(Fonts::getFont(NUNITO_BOLD));
     welcome.setString("Welcome");
-    welcome.setCharacterSize(100);
+    welcome.setCharacterSize(150);
     welcome.setFillColor({175, 143, 54});
-    welcome.setPosition(50, 1180);
+    welcome.setPosition(50, 1125);
 
     getStarted.setFont(Fonts::getFont(NUNITO_BOLD));
     getStarted.setString("Get Started");
     getStarted.setCharacterSize(55);
     getStarted.setFillColor({151, 151,151});
-    getStarted.setPosition(80, 1310);
+    getStarted.setPosition(85, 1310);
 
 }
 
@@ -79,6 +79,10 @@ void SplashScreen::eventHandler(sf::RenderWindow &window, const sf::Event &event
         animationClock.restart(); // Start the clock when the start button is clicked
         std::cout << "START CLICKED";
 
+    }else if (MouseEvents::isHover(startButton.getGlobalBounds(), window) ){
+        startButton.setFillColor({180,180,180});
+    }else {
+        startButton.setFillColor({151, 151,151});
     }
     if(MouseEvents::isHover(musicMixer.getGlobalBounds(), window)){
         musicMixer.setState(HOVERED, true);
@@ -113,21 +117,22 @@ void SplashScreen::draw(sf::RenderTarget &target, sf::RenderStates states) const
     target.draw(welcome, states);
     target.draw(getStarted,states);
 
+    sf::RenderStates newStates = states;
+
     if(musicPlayer.checkStates(HOVERED)){
-        states.transform.scale(1.1,1.1);
-        target.draw(musicPlayer,states);
-    }else{
-        states.transform.scale(1,1);
-        target.draw(musicPlayer,states);
+        sf::FloatRect s = musicPlayer.getGlobalBounds();
+        newStates.transform.scale(1.025,1.025,s.left + s.width/2,s.top + s.height/2);
     }
+    target.draw(musicPlayer,newStates);
+
+    newStates = states;
 
     if(musicMixer.checkStates(HOVERED)){
-        states.transform.scale(1.1,1.1);
-        target.draw(musicMixer,states);
-    }else{
-        states.transform.scale(1,1);
-        target.draw(musicMixer,states);
+        sf::FloatRect s = musicMixer.getGlobalBounds();
+        newStates.transform.scale(1.025,1.025,s.left + s.width/2,s.top + s.height/2);
     }
+    target.draw(musicMixer,newStates);
+
 
 }
 
@@ -141,11 +146,11 @@ void SplashScreen::fall() {
 
     // Calculate the new position of pro based on the elapsed time and fall duration
     float startY = -pro.getGlobalBounds().height - 180;
-    float targetY = dizzleStudio.getPosition().y;
+    float targetY = dizzleStudio.getPosition().y - 10;
     float currentY = startY + ((targetY - startY) * (elapsedTime / duration));
 
     // Gradually move dizzleStudio horizontally according to the fallDuration
-    float movementDistance = -160;
+    float movementDistance = -270;
     float currentX = initialDizzleStudioPosition.x + (movementDistance * (elapsedTime / duration));
     dizzleStudio.setPosition({currentX, dizzleStudio.getPosition().y});
 
